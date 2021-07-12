01113ef2da2cbed04a40ed0ca1160ca261516b93
NyCode <nico@nycode.de>
2021-07-12 23:50:56 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ repositories {
 dependencies {
     implementation("dev.schlaubi", "envconf", "1.0")
     implementation("org.slf4j", "slf4j-api", "1.7.30")
+    implementation("de.nycode:retrofit2-kotlinx-serialization-converter:0.9.1")
 }
 
